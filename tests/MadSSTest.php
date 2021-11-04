79e8e1a19353d622ddd7cb2f6d7eb1acca26a250
Oliver Etchebarne <yo@drmad.org>
2021-11-04 00:46:46 -0500

    Initial tests

---
+++ tests/MadSSTest.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+use Vendimia\MadSS\MadSS;
+
+final class MadSSTest extends TestCase
+{
+    public function testSimpleCss()
+    {
+        $css = "/*Esto es un comentario*/body {
+            background: red;
+        }";
+        $expected = "body{background:red}";
+
+        $madss = new MadSS;
+        $this->assertEquals(
+            $expected,
+            $madss->processString($css, 'css'),
+        );
+    }
+
+    public function testDeepSelectors()
+    {
+        $css = "body { p { strong { color: red; }}}";
+        $expected = "body p strong{color:red}";
+
+        $madss = new MadSS;
+        $this->assertEquals(
+            $expected,
+            $madss->processString($css, 'css'),
+        );
+    }
+
+    public function testStringInDeclaration() {
+        $css = "p::before{content: '{message}'}";
+        $expected = "p::before{content:'{message}'}";
+
+        $madss = new MadSS;
+        $this->assertEquals(
+            $expected,
+            $madss->processString($css, 'css'),
+        );
+    }
+
+    public function testChildWithParentWithMultipleSelector()
+    {
+        $css = "a, b { color: red; &:hover {color: blue}}";
+        $expected = "a,b{color:red}\na:hover,b:hover{color:blue}";
+
+        $madss = new MadSS;
+        $this->assertEquals(
+            $expected,
+            $madss->processString($css, 'css'),
+        );
+    }
+
+}
